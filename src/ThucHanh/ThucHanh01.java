0ff28c0694273943fa92bb789862a97ca7f45407
DoChuan13 <chuando.9213@gmail.com>
2023-03-02 19:28:32 +0700

    Show System Time

---
+++ src/ThucHanh/ThucHanh01.java
@@ -0,0 +1,11 @@
+package ThucHanh;
+
+import java.util.Date;
+
+public class ThucHanh01 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Date date = new Date();
+        System.out.println("Time now: "+date);
+    }
+}
